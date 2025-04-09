131a75abdfb79754db9fb9d0a6bee0598e0633a9
Kau814 <k3392677@gmail.com>
2025-04-09 13:04:13 +0000

    novo commit novato adicionei printf e scanf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,32 +1,49 @@
 #include <stdio.h>
 
-int main() {
-    int População, NPT;
+int main(){
+
+    int NPT;
+    int populacao;
     float KM, PIB;
     char estado;
     char cidade[20];
-    char código[5];
+    char codigo[5];
 
-    printf("Digite seu Estado: \n");
+    printf("*** Bem vindo ao Super Trunfo! ***\n");
+
+
+    printf("Digite o Estado da Carta 1: \n");
     scanf("%s", &estado);
-
-    printf("Digite seu Código: \n");
-    scanf("%s", &código);
-
-    printf("Digite sua Cidade: \n");
+    printf("Digite o nome da Cidade da carta 1: \n");
     scanf("%s", &cidade);
-
-    printf("Digite sua Área em KM²: \n");
+    printf("Digite o códido da Carta 1: \n");
+    scanf("%s", &codigo);
+    printf("Digite a área em KM² da Carta 1: \n");
     scanf("%f", &KM);
-
-    printf("Digite sua População: \n");
-    scanf("%f", &População);
-
-    printf("Digite seus Pontos Turísticos: \n");
+    printf("Digite a População da Carta 1: \n");
+    scanf("%f", &populacao);
+    printf("Digite a quantia de Pontos Turísticos da Carta 1: \n");
     scanf("%d", &NPT);
+    printf("Digite o PIB da sua Carta 1: \n");
+    scanf("%f", &PIB);
 
-    printf("Digite o PIB: \n");
+
+
+    printf("Digite o Estado da Carta 2: \n");
+    scanf("%s", &estado);
+    printf("Digite o nome da Cidade da carta 2: \n");
+    scanf("%s", &cidade);
+    printf("Digite o códido da Carta 2: \n");
+    scanf("%s", &codigo);
+    printf("Digite a área em KM² da Carta 2: \n");
+    scanf("%f", &KM);
+    printf("Digite a População da Carta 2: \n");
+    scanf("%f", &populacao);
+    printf("Digite a quantia de Pontos Turísticos da Carta 2: \n");
+    scanf("%d", &NPT);
+    printf("Digite o PIB da sua Carta 2: \n");
     scanf("%f", &PIB);
 
     return 0;
+
 }
